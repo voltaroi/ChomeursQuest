4212b572f63600a54c3258e9fac52a4fa55b667b
voltaroi <139485904+voltaroi@users.noreply.github.com>
2025-02-28 15:39:27 +0100

    add readFile

--- src/application/Game/Attack.java
+++ src/application/Game/Attack.java
@@ -1,5 +1,9 @@
 package application.Game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Attack {
 	private String name;
 	private boolean isAttackSpe = false;
@@ -7,15 +11,47 @@ public class Attack {
 	private String type;
 	private String effect;
 	
-	public Attack(String name, boolean isAttackSpe, float att, String type, String effect) {
-		this.name = name;
-		this.isAttackSpe = isAttackSpe;
-		this.att = att;
-		this.type = type;
-		this.effect = effect;
-	}
-	
 	public String getName() {
 		return name;
 	}
+	
+    public void FromFile(String filePath) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("=");
+                if (parts.length == 2) {
+                    switch (parts[0]) {
+                        case "name":
+                            name = parts[1];
+                            break;
+                        case "isAttackSpe":
+                            isAttackSpe = Boolean.parseBoolean(parts[1]);
+                            break;
+                        case "att":
+                            att = Float.parseFloat(parts[1]);
+                            break;
+                        case "type":
+                            type = parts[1];
+                            break;
+                        case "effect":
+                            effect = parts[1];
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getString() {
+        return "name=" + name + "\n" +
+               "isAttackSpe=" + isAttackSpe + "\n" +
+               "att=" + att + "\n" +
+               "type=" + type + "\n" +
+               "effect=" + effect;
+    }
+
 }
--- src/application/controllers/GameController.java
+++ src/application/controllers/GameController.java
@@ -1,18 +1,10 @@
 package application.controllers;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 import application.Game.Attack;
 import application.Game.Player;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
 
 public class GameController {
 	
@@ -26,49 +18,16 @@ public class GameController {
     
     @FXML
     public void initialize() {
+    	Attack attack = new Attack();
+    	String name = "attackTest";
+    	attack.FromFile("C:\\Users\\volta\\eclipse-workspace\\ChomeursQuest\\src\\assets\\" + name +".txt");
+    	System.out.println(attack.getString());
     	
-    	readAttackFromFile("d");
+    	player1 = new Player();
+    	player2 = new Player();
     	
     	player1.addChomeurs(null);
     	player2.addChomeurs(null);
     	
     }
-    
-    public Attack readAttackFromFile(String filePath) {
-        String name = "";
-        boolean isAttackSpe = false;
-        float att = 0;
-        String type = "";
-        String effect = "";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split("=");
-                if (parts.length == 2) {
-                    switch (parts[0]) {
-                        case "name":
-                            name = parts[1];
-                            break;
-                        case "isAttackSpe":
-                            isAttackSpe = Boolean.parseBoolean(parts[1]);
-                            break;
-                        case "att":
-                            att = Float.parseFloat(parts[1]);
-                            break;
-                        case "type":
-                            type = parts[1];
-                            break;
-                        case "effect":
-                            effect = parts[1];
-                            break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Attack(name, isAttackSpe, att, type, effect);
-    }
-
 }
